0040ab70456c34463922012e69b22fd2e9c60be9
Abraomukas <abraomukas@gmail.com>
2022-10-10 23:57:41 +0400

    feat: refactor code, new approach

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
+import React, { useState } from 'react';
+
 import ContentLeft from './components/ContentLeft';
 import ContentMiddle from './components/ContentMiddle';
 import ContentRight from './components/ContentRight';
 import ContentTop from './components/ContentTop';
 
+import { OptionsProvider } from './contexts/OptionsContext';
+
 export default function App() {
+	const [options, setOptions] = useState([]);
+
 	return (
 		<div className='container-fluid' style={{ padding: '0 0 0 0' }}>
 			<ContentTop />
 			<div
 				className='container-fluid'
 				style={{ backgroundColor: 'green', height: '80vh' }}>
+				{/* <div className='row'>
+					<OptionsProvider>
+						<ContentLeft />
+						<ContentMiddle />
+						<ContentRight />
+					</OptionsProvider>
+				</div> */}
 				<div className='row'>
-					<ContentLeft />
-					<ContentMiddle />
-					<ContentRight />
+					<Content />
 				</div>
 			</div>
 		</div>
